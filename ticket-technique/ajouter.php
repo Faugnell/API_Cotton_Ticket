b6e153ee31a33b2d94e128bd1acfe41885947f51
Faugnell <90843661+Faugnell@users.noreply.github.com>
2023-05-26 11:19:36 +0200

    header("Access-Control-Allow-Methods: PUT");

--- ticket-technique/ajouter.php
+++ ticket-technique/ajouter.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 // Méthode autorisée
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: PUT");
 
 // Durée de vie de la requête
 header("Access-Control-Max-Age: 3600");
